be4a50118981446f0ba72bac8ca9a3b3b3e555f6
Gustavo Souza <gustavo_souzapr@outlook.com>
2025-10-08 19:54:17 -0300

    Feat: Terminando o CRUD

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,16 +23,22 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.GB_Produto = New System.Windows.Forms.GroupBox()
+        Me.TX_ProdutoId = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.TX_NomeProduto = New System.Windows.Forms.TextBox()
         Me.LB_Nome = New System.Windows.Forms.Label()
-        Me.BT_Cadastrar = New System.Windows.Forms.Button()
+        Me.BT_Salvar = New System.Windows.Forms.Button()
         Me.DG_Produtos = New System.Windows.Forms.DataGridView()
+        Me.BT_Limpar = New System.Windows.Forms.Button()
+        Me.BT_Excluir = New System.Windows.Forms.Button()
         Me.GB_Produto.SuspendLayout()
         CType(Me.DG_Produtos, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'GB_Produto
         '
+        Me.GB_Produto.Controls.Add(Me.TX_ProdutoId)
+        Me.GB_Produto.Controls.Add(Me.Label1)
         Me.GB_Produto.Controls.Add(Me.TX_NomeProduto)
         Me.GB_Produto.Controls.Add(Me.LB_Nome)
         Me.GB_Produto.Location = New System.Drawing.Point(10, 21)
@@ -41,6 +47,24 @@ Partial Class Form1
         Me.GB_Produto.TabIndex = 0
         Me.GB_Produto.TabStop = False
         Me.GB_Produto.Text = "Cadastro de Produto"
+        '
+        'TX_ProdutoId
+        '
+        Me.TX_ProdutoId.Enabled = False
+        Me.TX_ProdutoId.Location = New System.Drawing.Point(300, 17)
+        Me.TX_ProdutoId.Name = "TX_ProdutoId"
+        Me.TX_ProdutoId.Size = New System.Drawing.Size(51, 20)
+        Me.TX_ProdutoId.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.DarkBlue
+        Me.Label1.Location = New System.Drawing.Point(273, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(21, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "ID:"
         '
         'TX_NomeProduto
         '
@@ -59,34 +83,61 @@ Partial Class Form1
         Me.LB_Nome.TabIndex = 0
         Me.LB_Nome.Text = "Nome:"
         '
-        'BT_Cadastrar
+        'BT_Salvar
         '
-        Me.BT_Cadastrar.BackColor = System.Drawing.SystemColors.ControlLight
-        Me.BT_Cadastrar.ForeColor = System.Drawing.Color.CornflowerBlue
-        Me.BT_Cadastrar.Location = New System.Drawing.Point(643, 38)
-        Me.BT_Cadastrar.Name = "BT_Cadastrar"
-        Me.BT_Cadastrar.Size = New System.Drawing.Size(111, 49)
-        Me.BT_Cadastrar.TabIndex = 1
-        Me.BT_Cadastrar.Text = "Cadastrar"
-        Me.BT_Cadastrar.UseMnemonic = False
-        Me.BT_Cadastrar.UseVisualStyleBackColor = False
+        Me.BT_Salvar.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.BT_Salvar.ForeColor = System.Drawing.Color.CornflowerBlue
+        Me.BT_Salvar.Location = New System.Drawing.Point(643, 38)
+        Me.BT_Salvar.Name = "BT_Salvar"
+        Me.BT_Salvar.Size = New System.Drawing.Size(111, 49)
+        Me.BT_Salvar.TabIndex = 1
+        Me.BT_Salvar.Text = "Salvar"
+        Me.BT_Salvar.UseMnemonic = False
+        Me.BT_Salvar.UseVisualStyleBackColor = False
         '
         'DG_Produtos
         '
         Me.DG_Produtos.AllowUserToAddRows = False
+        Me.DG_Produtos.AllowUserToDeleteRows = False
         Me.DG_Produtos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DG_Produtos.Location = New System.Drawing.Point(10, 218)
         Me.DG_Produtos.Name = "DG_Produtos"
         Me.DG_Produtos.Size = New System.Drawing.Size(778, 220)
         Me.DG_Produtos.TabIndex = 2
         '
+        'BT_Limpar
+        '
+        Me.BT_Limpar.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.BT_Limpar.ForeColor = System.Drawing.Color.CornflowerBlue
+        Me.BT_Limpar.Location = New System.Drawing.Point(657, 93)
+        Me.BT_Limpar.Name = "BT_Limpar"
+        Me.BT_Limpar.Size = New System.Drawing.Size(83, 32)
+        Me.BT_Limpar.TabIndex = 3
+        Me.BT_Limpar.Text = "Novo / Limpar"
+        Me.BT_Limpar.UseMnemonic = False
+        Me.BT_Limpar.UseVisualStyleBackColor = False
+        '
+        'BT_Excluir
+        '
+        Me.BT_Excluir.BackColor = System.Drawing.Color.MistyRose
+        Me.BT_Excluir.ForeColor = System.Drawing.Color.OrangeRed
+        Me.BT_Excluir.Location = New System.Drawing.Point(10, 189)
+        Me.BT_Excluir.Name = "BT_Excluir"
+        Me.BT_Excluir.Size = New System.Drawing.Size(80, 23)
+        Me.BT_Excluir.TabIndex = 4
+        Me.BT_Excluir.Text = "Excluir"
+        Me.BT_Excluir.UseMnemonic = False
+        Me.BT_Excluir.UseVisualStyleBackColor = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.BT_Excluir)
+        Me.Controls.Add(Me.BT_Limpar)
         Me.Controls.Add(Me.DG_Produtos)
-        Me.Controls.Add(Me.BT_Cadastrar)
+        Me.Controls.Add(Me.BT_Salvar)
         Me.Controls.Add(Me.GB_Produto)
         Me.Name = "Form1"
         Me.Text = "Cadastro de  Produtos"
@@ -100,6 +151,10 @@ Partial Class Form1
     Friend WithEvents GB_Produto As GroupBox
     Friend WithEvents TX_NomeProduto As TextBox
     Friend WithEvents LB_Nome As Label
-    Friend WithEvents BT_Cadastrar As Button
+    Friend WithEvents BT_Salvar As Button
     Friend WithEvents DG_Produtos As DataGridView
+    Friend WithEvents TX_ProdutoId As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BT_Limpar As Button
+    Friend WithEvents BT_Excluir As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,30 +1,54 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class Form1
-    Private Sub BT_Cadastrar_Click(sender As Object, e As EventArgs) Handles BT_Cadastrar.Click
-        Dim connectionString As String = "Server=127.0.0.1;Database=estoque_wms;Uid=root;Pwd=;"
+    Private Sub BT_Salvar_Click(sender As Object, e As EventArgs) Handles BT_Salvar.Click
+        If String.IsNullOrEmpty(TX_ProdutoId.Text) Then
+            Dim connectionString As String = "Server=127.0.0.1;Database=estoque_wms;Uid=root;Pwd=;"
 
-        Try
-            Using conn As New MySqlConnection(connectionString)
-                conn.Open()
+            Try
+                Using conn As New MySqlConnection(connectionString)
+                    conn.Open()
 
-                Dim sql As String = "INSERT INTO produtos (nome, data_cadastro) VALUES (@nome, @data_cadastro);"
-                Dim cmd As New MySqlCommand(sql, conn)
+                    Dim sql As String = "INSERT INTO produtos (nome, data_cadastro) VALUES (@nome, @data_cadastro);"
+                    Dim cmd As New MySqlCommand(sql, conn)
 
-                cmd.Parameters.AddWithValue("@nome", TX_NomeProduto.Text)
-                cmd.Parameters.AddWithValue("@data_cadastro", DateTime.Now)
+                    cmd.Parameters.AddWithValue("@nome", TX_NomeProduto.Text)
+                    cmd.Parameters.AddWithValue("@data_cadastro", DateTime.Now)
 
-                cmd.ExecuteNonQuery()
+                    cmd.ExecuteNonQuery()
 
-                MessageBox.Show("Produto cadastrado com sucesso!")
+                    MessageBox.Show("Produto cadastrado com sucesso!")
 
-                TX_NomeProduto.Clear()
-                TX_NomeProduto.Focus()
-                CarregarProdutos()
-            End Using
-        Catch ex As Exception
-            MessageBox.Show("Falha ao cadastrar o produto." & vbCrLf & "Erro: " & ex.Message)
-        End Try
+                    TX_NomeProduto.Clear()
+                    TX_NomeProduto.Focus()
+                    CarregarProdutos()
+                End Using
+            Catch ex As Exception
+                MessageBox.Show("Falha ao cadastrar o produto." & vbCrLf & "Erro: " & ex.Message)
+            End Try
+
+        Else
+            Dim connectionString As String = "Server=127.0.0.1;Database=estoque_wms;Uid=root;Pwd=;"
+
+            Try
+                Using conn As New MySqlConnection(connectionString)
+                    conn.Open()
+                    Dim sql As String = "UPDATE produtos SET nome = @nome WHERE id = @id;"
+                    Dim cmd As New MySqlCommand(sql, conn)
+                    cmd.Parameters.AddWithValue("@nome", TX_NomeProduto.Text)
+                    cmd.Parameters.AddWithValue("@id", Convert.ToInt32(TX_ProdutoId.Text))
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Produto atualizado com sucesso!")
+                    TX_ProdutoId.Clear()
+                    TX_NomeProduto.Clear()
+                    TX_NomeProduto.Focus()
+                    CarregarProdutos()
+                End Using
+            Catch ex As Exception
+                MessageBox.Show("Falha ao atualizar o produto." & vbCrLf & "Erro: " & ex.Message)
+            End Try
+        End If
+
     End Sub
 
     Private Sub CarregarProdutos()
@@ -49,5 +73,58 @@ Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         CarregarProdutos()
+    End Sub
+
+
+
+    Private Sub BT_Limpar_Click(sender As Object, e As EventArgs) Handles BT_Limpar.Click
+        TX_ProdutoId.Clear()
+        TX_NomeProduto.Clear()
+        TX_NomeProduto.Focus()
+    End Sub
+
+    Private Sub DG_Produtos_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DG_Produtos.CellClick
+        If e.RowIndex >= 0 Then
+
+            Dim linhaSelecionada As DataGridViewRow = DG_Produtos.Rows(e.RowIndex)
+
+            TX_NomeProduto.Text = linhaSelecionada.Cells("nome").Value.ToString()
+            TX_ProdutoId.Text = linhaSelecionada.Cells("ID").Value.ToString()
+
+
+        End If
+    End Sub
+
+    Private Sub BT_Excluir_Click(sender As Object, e As EventArgs) Handles BT_Excluir.Click
+        If String.IsNullOrEmpty(TX_ProdutoId.Text) Then
+            MessageBox.Show("Selecione um produto para excluir.")
+            Return
+        End If
+
+        If MessageBox.Show("Tem certeza que desea excluir este produto?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) = DialogResult.Yes Then
+
+            Dim connectionString As String = "Server=127.0.0.1;Database=estoque_wms;Uid=root;Pwd=;"
+
+            Try
+                Using conn As New MySqlConnection(connectionString)
+                    conn.Open()
+                    Dim sql As String = "DELETE FROM produtos WHERE id = @id;"
+                    Dim cmd As New MySqlCommand(sql, conn)
+
+
+                    cmd.Parameters.AddWithValue("@id", Convert.ToInt32(TX_ProdutoId.Text))
+
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Produto excluído com sucesso!")
+
+                End Using
+
+            Catch ex As Exception
+                MessageBox.Show("Falha ao excluir o produto." & vbCrLf & "Erro: " & ex.Message)
+            End Try
+
+            BT_Limpar_Click(Nothing, Nothing)
+            CarregarProdutos()
+        End If
     End Sub
 End Class
